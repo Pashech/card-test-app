0ebf2500c99907d8ac9fb9c38ebf08dab860b71f
Pashech <Pashech555@gmail.com>
2025-04-29 15:53:55 +0300

    add dependencies

---
+++ src/main/java/com/example/card_test_app/CardTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.card_test_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardTestAppApplication.class, args);
+	}
+
+}
