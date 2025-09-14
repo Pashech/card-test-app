6e08d4c38de494bb7c7234893641378c32aa1833
Pashech <Pashech555@gmail.com>
2025-09-14 18:32:14 +0300

    add for

--- src/main/java/com/example/card_test_app/CardTestAppApplication.java
+++ src/main/java/com/example/card_test_app/CardTestAppApplication.java
@@ -10,6 +10,10 @@ public class CardTestAppApplication {
 		SpringApplication.run(CardTestAppApplication.class, args);
         System.out.println("Hello");
 
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+
 	}
 
 }
