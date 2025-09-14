bda8dac791134808abacac10c3e4ede6b516b222
Pashech <Pashech555@gmail.com>
2025-09-14 18:44:46 +0300

    asffdsf

--- src/main/java/com/example/card_test_app/CardTestAppApplication.java
+++ src/main/java/com/example/card_test_app/CardTestAppApplication.java
@@ -10,27 +10,8 @@ public class CardTestAppApplication {
 		SpringApplication.run(CardTestAppApplication.class, args);
         System.out.println("Hello");
 
-        int i = 0;
-        while(i < 4) {
-            System.out.println(i);
-            i++;
-        }
 
-        for(int j =0; j < 2; j++) {
-            System.out.println("Hello");
-        }
-
-        for (int i = 0; i < 5; i++) {
-            System.out.println(i);
-        }
-
-        for (int i = 0; i < 5; i++) {
-            System.out.println(i);
-        }
-
-        for (int i = 0; i < 5; i++) {
-            System.out.println(i);
-        }
+        System.out.println("skdgsljfgkdjfghkfdjgn");
 
 	}
 
